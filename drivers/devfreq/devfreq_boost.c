f2a49f396b100956b13d58bbfb8e0df90617513c
Andrea <linckandrea01@gmail.com>
2020-10-26 16:18:59 +0100

    devfreq_boost: expose compile some time settings as module parameters

--- drivers/devfreq/devfreq_boost.c
+++ drivers/devfreq/devfreq_boost.c
@@ -9,6 +9,12 @@
 #include <linux/fb.h>
 #include <linux/input.h>
 #include <linux/kthread.h>
+#include <linux/moduleparam.h>
+
+static unsigned short devfreq_wake_boost_duration_ms __read_mostly =
+	CONFIG_DEVFREQ_WAKE_BOOST_DURATION_MS;
+static unsigned short devfreq_input_boost_duration_ms __read_mostly =
+	CONFIG_DEVFREQ_INPUT_BOOST_DURATION_MS;
 
 enum {
 	SCREEN_OFF,
@@ -58,7 +64,7 @@ static void __devfreq_boost_kick(struct boost_dev *b)
 
 	set_bit(INPUT_BOOST, &b->state);
 	if (!mod_delayed_work(system_unbound_wq, &b->input_unboost,
-		msecs_to_jiffies(CONFIG_DEVFREQ_INPUT_BOOST_DURATION_MS)))
+		msecs_to_jiffies(devfreq_input_boost_duration_ms)))
 		wake_up(&b->boost_waitq);
 }
 
@@ -192,7 +198,7 @@ static int fb_notifier_cb(struct notifier_block *nb, unsigned long action,
 		if (*blank == FB_BLANK_UNBLANK) {
 			clear_bit(SCREEN_OFF, &b->state);
 			__devfreq_boost_kick_max(b,
-				CONFIG_DEVFREQ_WAKE_BOOST_DURATION_MS);
+				devfreq_wake_boost_duration_ms);
 		} else {
 			set_bit(SCREEN_OFF, &b->state);
 			wake_up(&b->boost_waitq);
